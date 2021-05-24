2d895cd957757f4a12062ff17ecc8103516a0d40
Matthew Luedtke <MLuedtke@tql.com>
2021-05-24 16:34:38 -0400

    Add project files.

---
+++ GenericCollectionLesson/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCollectionLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng = new Random();
+            List<int> scores = new List<int>();
+            var total = 0;
+            for (var i = 1; i < 31; i++){
+                scores.Add(rng.Next(0, 31));
+            }
+
+            foreach(var j in scores)
+            {
+                total += j;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
